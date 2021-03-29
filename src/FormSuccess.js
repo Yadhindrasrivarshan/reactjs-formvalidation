3cbfb10a6c3b248fbdb7702a39c86a0abac4dd07
Yadhindrasrivarshan <YadhiPA@eu.trimblecorp.net>
2021-03-29 15:26:51 +0530

    Deploy my react app

---
+++ src/FormSuccess.js
@@ -0,0 +1,15 @@
+
+
+const FormSuccess =()=>{
+   return(
+       <div className="form-content-right">
+           <div className="form-success">
+               We Have Received your details
+           </div>
+           <img src="img/img-3.svg" alt='success-image' className="form-img-2"/>
+       </div>
+   )
+} 
+
+
+export default FormSuccess
---
+++ src/validateInfo.js
@@ -0,0 +1,33 @@
+export default function validateInfo(values){
+    let errors={}
+    if(!values.username.trim()){
+        errors.username="Username required"
+    }
+
+   if(!values.email){
+       errors.email="Email is required"
+   }
+   else if(!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = 'Email address is invalid';
+  }
+
+
+  if(!values.password){
+      errors.password="Password is required"
+  
+  }
+  else if(values.password.length<6){
+      errors.password="Password need to be 6 characters or more"
+  }
+
+
+  if(!values.password2){
+      errors.password2="Password is required"
+
+  }
+  else if(values.password!==values.password2){
+      errors.password2="Password doesnt matches"
+  }
+  return errors
+
+}
